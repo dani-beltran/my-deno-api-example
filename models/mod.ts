0bf31cf35f4ce4047a42be746dc9bc249d0dd262
Daniel Beltran <danilidonbeltran@gmail.com>
2021-09-20 21:15:16 +0200

    restored player model registration

--- models/mod.ts
+++ models/mod.ts
@@ -1,5 +1,6 @@
 import { Database } from "../deps.ts";
 import { Pet } from "./Pet.ts";
+import { Player } from "./Player.ts";
 
 /**
  * Register the models in the app server's DB.
@@ -10,5 +11,6 @@ export function registerModels(db: Database) {
   // it is good practice to put them first
   db.link([
     Pet,
+    Player,
   ]);
 }
